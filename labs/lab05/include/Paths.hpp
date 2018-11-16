27cbb132716154059842a8a67e018f7b3b052f3a
yaoyao0821 <jxueyao@gmail.com>
2018-11-16 02:48:03 -0800

    change path

--- labs/lab05/include/Paths.hpp
+++ labs/lab05/include/Paths.hpp
@@ -2,6 +2,6 @@
 
 namespace lab5
 {
-    constexpr const char ShaderDirectory[] = "/Users/mac/Documents/CSC578Lab/1031/csc473_fall2018_labs/labs/lab05/shaders/";
-    constexpr const char DataDirectory[] = "/Users/mac/Documents/CSC578Lab/1031/csc473_fall2018_labs/data/";
+    constexpr const char ShaderDirectory[] = "/Users/mac/Documents/workspace2018/Spring-Cart-System-In-Lagrange/labs/lab05/shaders/";
+    constexpr const char DataDirectory[] = "/Users/mac/Documents/workspace2018/Spring-Cart-System-In-Lagrange/data/";
 }
